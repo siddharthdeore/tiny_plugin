9627779c61620d2bb67be4baebd7f2763e8a3161
Siddharth Deore <siddharth.deore@iit.it>
2023-11-17 00:50:11 +0100

    cross platform macros for export api

--- include/tiny_plugin/plugin_helper.h
+++ include/tiny_plugin/plugin_helper.h
@@ -17,19 +17,28 @@
     #define LIB_OS_PREFIX "lib"
 #endif
 
-#ifdef _WIN32
-    // Windows
+
+#if defined(_WIN32)
     #ifdef EXPORT_PLUGIN_DLL
         #define EXPORT_SYMBOL __declspec(dllexport) extern "C"
     #else
         #define EXPORT_SYMBOL __declspec(dllimport) extern "C"
     #endif
-#elif defined(__GNUC__)
-    // Linux/GCC
-//     #define EXPORT_SYMBOL extern "C" __attribute__((visibility("default")))
-// #else
-    // Default fallback
-    #define EXPORT_SYMBOL extern "C"
+#elif defined(__GNUC__) || defined(__clang__)
+    #define EXPORT_SYMBOL extern "C" __attribute__((visibility("default")))
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+    #define EXPORT_SYMBOL extern "C" __declspec(dllexport) extern "C"
+#elif defined(__CYGWIN__)
+    #define EXPORT_SYMBOL extern "C" __declspec(dllexport) extern "C"
+#elif defined(__APPLE__)
+    #include <TargetConditionals.h>
+    #if TARGET_OS_MAC
+        #define EXPORT_SYMBOL extern "C" __attribute__((visibility("default")))
+    #endif
+#elif defined(__sun) && defined(__SVR4)
+    #define EXPORT_SYMBOL extern "C" __global
+#else
+    #error "Unsupported compiler/platform"
 #endif
 
 // clang-format on
